8d9e96192f7303b43c0a2fe10eadadb866f0b43f
Josh Granek <josh@duke.edu>
2015-04-09 13:50:13 -0400

    plot_metaclusters.py: Old change! I think this change alters the x-axis tick labels

--- make_synthetic/scripts/plot_metaclusters.py
+++ make_synthetic/scripts/plot_metaclusters.py
@@ -2,7 +2,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
-from matplotlib.backends.backend_pdf import PdfPages
+# from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.ticker import FuncFormatter, MaxNLocator
+
 import os
 
 READ_HEIGHT = 1
@@ -27,39 +29,9 @@ def plot_clusters(metacluster_list,plotfilename):
         ax.set_xlim(metacluster.iv.start,metacluster.iv.end)
         ax.set_ylim(1,stack_count*READ_PITCH)
         ax.set_xlabel(metacluster.iv.chrom)
+        ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: '%.0f'%x))
         xmin, xmax = ax.get_xaxis().get_view_interval()
         ymin, ymax = ax.get_yaxis().get_view_interval()
         ax.add_artist(Line2D((xmin, xmax), (ymin, ymin), color='black', linewidth=2))
         plt.tight_layout()
         plt.savefig("{0}_{1}{2}".format(plotbase,i,plotext))
-
-        #============================================================
-        # fig1 = plt.figure(facecolor='white')
-        # ax1 = plt.axes(frameon=False)
-
-        # #ax1.set_frame_on(False) # Alternate way to turn frame off
-
-        # ax1.get_xaxis().tick_bottom() # Turn off ticks at top of plot
-
-        # #ax1.axes.get_xaxis().set_visible(False)
-        # ax1.axes.get_yaxis().set_visible(False) # Hide y axis
-
-        # # Add a plot
-        # y_offset = 2.0
-        # x = numpy.arange(-5.0, 5.0, 0.1)
-        # for i in range(3):
-        #     ax1.plot(x, numpy.sin((i + 1) * x) + i * y_offset, label=str(i+1))
-
-        # plt.legend(loc='lower right')
-
-        # # Draw the x axis line
-        # # Note that this must be done after plotting, to get the correct
-        # # view interval
-        # xmin, xmax = ax1.get_xaxis().get_view_interval()
-        # ymin, ymax = ax1.get_yaxis().get_view_interval()
-        # ax1.add_artist(Line2D((xmin, xmax), (ymin, ymin), color='black', linewidth=2))
-        # #============================================================
-
-
-
-        
